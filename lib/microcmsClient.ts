5dc709a8d44f728ab6ce90526eb24e7316fda377
nisi0929 <kengo279@icloud.com>
2023-01-30 01:46:25 +0900

    microcmsデータの読み取り

--- lib/microcmsClient.ts
+++ lib/microcmsClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from 'microcms';
 import 'dotenv/load.ts';
+console.log(Deno.env.toObject());
 export const microcmsClient = createClient({
   serviceDomain: 'misodev',
   apiKey: `${Deno.env.get('MICROCMS_API_KEY')}`,
--- tasks/create_microcms_data.ts
+++ tasks/create_microcms_data.ts
@@ -1,9 +1,18 @@
 import { ensureDirSync } from 'fs';
 import { join } from 'path';
+import { createClient } from 'microcms';
 
-const data = {
-  message: 'data1',
-};
+export const microcmsClient = createClient({
+  serviceDomain: 'misodev',
+  apiKey: `${Deno.args[0]}`,
+});
+
+const articles = await microcmsClient
+  .get({ endpoint: 'articles' })
+  .then((res) => {
+    return res.contents;
+  });
+
 const data2 = {
   message: 'data2',
 };
@@ -13,7 +22,7 @@ const articlesPath = join(outputPath, 'articles.json');
 const careersPath = join(outputPath, 'careers.json');
 await Deno.writeFile(
   articlesPath,
-  new TextEncoder().encode(JSON.stringify(data, null, 2))
+  new TextEncoder().encode(JSON.stringify(articles, null, 2))
 );
 await Deno.writeFile(
   careersPath,
