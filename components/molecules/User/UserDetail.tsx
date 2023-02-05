19169ce1c8ea75c12d6203f02651662fe175890f
nisi0929 <kengo279@icloud.com>
2023-02-06 02:53:23 +0900

    message

--- components/molecules/User/UserDetail.tsx
+++ components/molecules/User/UserDetail.tsx
@@ -2,6 +2,9 @@ export const UserDetail = () => {
   return (
     <div class="my-5 ml-2">
       <p>B3、主にフロントエンド。興味のある技術はDeno,Freshです。</p>
+      <p>
+        株式会社はてなアルバイト中。株式会社キーザンキーザンでインターン中。
+      </p>
       <p>ラーメン,Apexが好き</p>
     </div>
   );
