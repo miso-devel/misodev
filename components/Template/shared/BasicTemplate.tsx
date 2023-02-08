a38319c6424e4a9193bf1b152e8d1c97a1565b33
nisi0929 <kengo279@icloud.com>
2023-02-08 09:36:05 +0900

    余白変更

--- components/Template/shared/BasicTemplate.tsx
+++ components/Template/shared/BasicTemplate.tsx
@@ -11,7 +11,7 @@ export const BasicTemplate = ({ children, path, title }: TProps) => {
   return (
     <main className="font-mono md:w-1/2 w-full  m-auto blur min-h-full md:border-r-4 md:border-l-4 border-white">
       <Header path={path} />
-      <TypoWrapper element="h1" className="text-center mt-10" word={title} />
+      <TypoWrapper element="h1" className="text-center my-10" word={title} />
       {children}
     </main>
   );
