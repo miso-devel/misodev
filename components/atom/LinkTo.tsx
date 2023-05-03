f6d315cdcd9c19b03b04ab66a7d28093e7a0e737
miso-devel <kengo279@icloud.com>
2023-05-03 21:56:06 +0900

    直接import

--- components/atom/LinkTo.tsx
+++ components/atom/LinkTo.tsx
@@ -1,4 +1,4 @@
-import { ComponentChildren } from 'https://esm.sh/v102/preact@10.11.0/src/index.d.ts';
+import { ComponentChildren } from 'preact';
 
 type TProps = {
   children: ComponentChildren;
@@ -8,7 +8,7 @@ export const LinkTo = ({ children, href }: TProps) => {
   return (
     <a
       href={href}
-      class="rounded-md py-2 px-4 border-2 border-gray-500 hover:border-black"
+      class='rounded-md py-2 px-4 border-2 border-gray-500 hover:border-black'
     >
       {children}
     </a>
