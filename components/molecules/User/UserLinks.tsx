58bed6e47fc849c6fecaa9c36973bb6491c8920f
miso-devel <kengo279@icloud.com>
2023-05-14 18:39:02 +0900

    formatterとlink書き換え

--- components/molecules/User/UserLinks.tsx
+++ components/molecules/User/UserLinks.tsx
@@ -1,10 +1,10 @@
-import { ImageLink } from '../../atom/ImageLink.tsx';
+import { ImageLink } from "../../atom/ImageLink.tsx";
 
 export const UserLinks = () => {
   return (
     <div class="flex px-2 gap-2">
       <ImageLink
-        href="https://twitter.com/donennnn"
+        href="https://twitter.com/mis0dev"
         imgLink="/images/Twitter.png"
       />
       <ImageLink
